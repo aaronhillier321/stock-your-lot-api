4846d42fd7bf32a15a57e38a8737a04a667425aa
Aaron Hillier <aaronhillier321@gmail.com>
2026-02-14 12:11:29 -0800

    required

--- src/main/resources/db/migration/V12__purchases_required_fields.sql
+++ src/main/resources/db/migration/V12__purchases_required_fields.sql
@@ -1,4 +1,12 @@
--- Require VIN, auction platform, purchase date, dealership, and purchase price on purchases.
+-- Delete rows where any of the soon-to-be-required fields are null.
+DELETE FROM purchases
+WHERE vin IS NULL
+   OR auction_platform IS NULL
+   OR purchase_date IS NULL
+   OR dealership_id IS NULL
+   OR purchase_price IS NULL;
+
+-- Require these fields.
 ALTER TABLE purchases ALTER COLUMN vin SET NOT NULL;
 ALTER TABLE purchases ALTER COLUMN auction_platform SET NOT NULL;
 ALTER TABLE purchases ALTER COLUMN purchase_date SET NOT NULL;
