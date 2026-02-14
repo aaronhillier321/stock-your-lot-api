f57e7a5e442a5e57961509dabb4fce4c04cde72f
Aaron Hillier <aaronhillier321@gmail.com>
2026-02-14 10:01:05 -0800

    file

--- src/main/java/com/stockyourlot/controller/FileMetadataController.java
+++ src/main/java/com/stockyourlot/controller/FileMetadataController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
